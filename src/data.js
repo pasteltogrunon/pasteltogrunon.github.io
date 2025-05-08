eb798a1ae0488600f418f7c24aeabbc1eee0fec7
pasteltogrunon <juanfernandezesteban@gmail.com>
2025-05-09 00:28:18 +0200

    Last test porque voy a llorar si no

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
       subtitle: "Infinite runner in a Moebius strip",
       description:
         "Hamptem the Hampster is a 3D infinite runner in a Moebius strip with the shape of a Bernoulli's lemniscate. We made it in a group of 8 people for a 3 day game jam. Its development was hard because of the technical complications of working with a non-orientable surface, but we managed to get it soomthly working for web. My roles for this project were: Gameplay and Tech Programming, and Technical, Environment and VFX Art. The game was made with Unity and C#.",
-      image: "public/projects/hamptem.png",
+      image: "public/hamptem.png",
       link: "https://adrianacevedoz.itch.io/hamptem-the-hamster",
     },
     {
@@ -12,7 +12,7 @@ export const projects = [
       subtitle: "City building card game",
       description:
         "The Dawn of the Colossus is a kingdom building card game. We made it in a group of 6 people for a 5 day game jam. To build a card game in such a short amount of time, the code needed to be really modular and easy to expand. My roles for this project were: Programming and Technical and VFX Art. The game was made with Unity and C#.",
-      image: "public/projects/dawn-of-the-colossus.png",
+      image: "public/dawn-of-the-colossus.png",
       link: "https://tomboygotchi.itch.io/dawn-of-the-colossus-the-colossus-dawn",
     },
     {
@@ -20,7 +20,7 @@ export const projects = [
       subtitle: "A metroidvania about the myth of Prometheus",
       description:
         "The Repentance of Prometheus is a story heavy metroidvania based on the titan Prometheus. We made it in a group of 8 people over a month and we tried a 2.5D artstyle. I took the lead of the project, and my other roles were: Programming, Character Design, Animation, VFX, Environment and Techical Art. The game was made with Unity and C#.",
-      image: "public/projects/prometeo.png",
+      image: "public/prometeo.png",
       link: "https://tomboygotchi.itch.io/prometheusisapussy",
     },
     {
@@ -28,7 +28,7 @@ export const projects = [
       subtitle: "Synthwave foddian plataformer",
       description:
         "Neon Pecker is a 2D synthwave foddian plataformer. I made it in 2023 with the help of a friend for my first game jam. Since we were only two, my roles for the project were: Programming, Art and Sound, UI and Game Design. The game was made with Unity and C#.",
-      image: "public/projects/neon-pecker.png",
+      image: "public/neon-pecker.png",
       link: "https://elpasteltogrunon.itch.io/neon-pecker",
     },
     {
@@ -36,7 +36,7 @@ export const projects = [
       subtitle: "A survivor-like game about possessing enemies",
       description:
         "In Reiko no Virus, you play as a virus anime girl that possesses enemies. This project was done with a great focus in a SOLID architectural design.",
-      image: "public/projects/reiko.png",
+      image: "public/reiko.png",
       link: "https://tomboygotchi.itch.io/reiko-no-virus-mutant-bio-menace",
     }
 
