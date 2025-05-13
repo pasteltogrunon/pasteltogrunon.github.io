ca6ff3e843d478c832d17bfa0e362c8e6968023e
pasteltogrunon <juanfernandezesteban@gmail.com>
2025-05-13 22:59:34 +0200

    Little typo fix

--- src/components/ArkOfMagic.jsx
+++ src/components/ArkOfMagic.jsx
@@ -12,7 +12,7 @@ export default function ArkOfMagic() {
                     Ark of Magic
                 </h1>
                 <p>
-                    Ark of magic is the biggest project I have worked on. It is a 3D, multiplayer (MOBA) game developed in Unity. I have been working on it since 2020, and it is the project I have learn Unity in. 
+                    Ark of magic is the biggest project I have worked on. It is a 3D, multiplayer (MOBA) game developed in Unity. I have been working on it since 2020, and it is the project with which I have learned Unity and C#. 
                     It has gone through multiple architectural changes, and I have covered almost all of the development, with some minor help from my friends.
                 </p>
             </div>
